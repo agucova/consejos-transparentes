ce87edf178499a802f9d329e0f68b67b74f13c1b
Agustín Covarrubias <agucova@uc.cl>
2020-08-10 03:22:46 -0400

    Procesamiento cliente para el API inicial

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-// Maneja el tipo
+var request = new XMLHttpRequest()
 
-var selectTipo = document.getElementById("tipo")
-selectTipo.addEventListener('change', actualizarRol)
+request.open("GET", "http://127.0.0.1:8000/consejo/generacional/")
+request.onload = function () {
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        window.datos = data
+        var selectTipo = document.getElementById("tipo")
+        var selectRol = document.getElementById("rol")
+        selectTipo.addEventListener('change', actualizarRol)
+        selectRol.addEventListener('change', mostrarAsistencia)
+        mostrarAsistencia();
+    } else {
+        console.log('No se pudo contactar la API de Consejos Transparentes.')
+    }
+}
+request.send()
+
+
+function remove_duplicates(arr) {
+    var obj = {};
+    var ret_arr = [];
+    for (var i = 0; i < arr.length; i++) {
+        obj[arr[i]] = true;
+    }
+    for (var key in obj) {
+        ret_arr.push(key);
+    }
+    return ret_arr;
+}
 
 function actualizarPlantilla(idElemento, datos) {
     const elemento = document.getElementById(idElemento);
@@ -10,12 +37,36 @@ function actualizarPlantilla(idElemento, datos) {
 }
 
 function actualizarRol() {
-    datos = {
-        tipo: "año",
-        roles: ["2020", "2019", "2018", "2017", "2016"]
+    console.log("actualizarRol");
+    const tipoRepresentante = document.getElementById("tipo").value;
+    const representantesConTipo = datos.filter(representante => representante.tipo == tipoRepresentante);
+    var tipo = null;
+    var roles = null;
+
+    if (tipoRepresentante == "DG") {
+        tipo = "año";
+        // Usar Set deduplica los años
+        roles = [...new Set(representantesConTipo.map(representante => representante.representa))];
     }
 
-    actualizarPlantilla('especificarRol', datos);
+    datosPlantilla = {
+        tipo: tipo,
+        roles: roles
+    }
+
+    actualizarPlantilla('especificarRol', datosPlantilla);
+    mostrarAsistencia();
 }
 
-actualizarRol();
+function mostrarAsistencia() {
+    console.log("mostrarAsistencia");
+    const tipoRepresentante = document.getElementById("tipo").value;
+    const rolRepresentante = document.getElementById("rol").value;
+    console.log(rolRepresentante);
+    const representantesF = datos.filter(representante => representante.representa == rolRepresentante);
+    console.log(representantesF);
+    representantesF.forEach(representante => function () {
+        console.log(representante.nombre);
+        console.log(representante.asistencias);
+    })
+}
